8ccaf9ea851a8855cafbc8a8aa3b91a04bd24c0b
Guy Rozendorn <guyr@infinidat.com>
2011-05-20 20:43:14 +0300

    getDescribe() is not inside the official gitpy, moving it

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 SETUP_INFO = dict(
     name = 'infi.recipe.template.version',
-    version = '0.3.1',
+    version = '0.3.2',
     author = 'Guy Rozendorn',
     author_email = 'guyr@infinidat.com',
 
